9a60ceb8914f06a7bb315c608263811520890d0a
Arman Babayan <armanbabayan2002@gmail.com>
2025-09-13 20:04:30 +0400

    homework

---
+++ Fibonacci/Fibonacci_matrix.cpp
@@ -0,0 +1,48 @@
+#include "Fibonacci.h"
+#include "parser.h"
+
+void to_upper (std::string& input)
+{
+    for(int i=0;i<input.size();i++)
+        input[i] = std::toupper(input[i]);
+} 
+
+int main ()
+{
+
+    std::cout  << "\n\n MATRIX  \n\n\n" ;
+
+    bool exit = 0;
+    number input;
+    std::string temp_input;
+
+    std::cout << "\n\n\t\t\tHello User !\n\n";
+    std::cout << "\t\t the program accepts unsigned integer numbers from 1:  \n";
+    std::cout << "\t\t if you want to exit write \"EXIT\" \n\n";
+
+    while(!exit)
+    {
+        input.is_not_num = 1;
+
+        while (input.is_not_num == 1)
+        {
+            std::cout << " \ninput :: -> ";
+            std::cin >> temp_input;
+            to_upper(temp_input);
+            if(temp_input == "EXIT")
+            {
+                exit = 1;
+                std::cout << "Bye Bye\n";
+                break;
+            }
+            input = string_to_int(temp_input); 
+            if(input.num <= 0 && input.is_not_num == 0) 
+                input.is_not_num = 1;
+        }
+
+        if(!exit)
+            std::cout  << "  " << input.num << "  index is : " << Fibonacci_matrix(input.num-1) << "\n";
+    }
+
+    return 0;
+}
